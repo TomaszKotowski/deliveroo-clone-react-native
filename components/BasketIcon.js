6ee47aa4d3c48545b26291f6ac96b7cc51cb4004
Tomasz Kotowski <tomasz.kotowski@abacai.com>
2022-07-25 15:07:36 +0200

    Create basket

--- components/BasketIcon.js
+++ components/BasketIcon.js
@@ -10,9 +10,14 @@ const BasketIcon = () => {
   const items = useSelector(selectBasketItems);
   const basketTotal = useSelector(selectBasketTotal);
 
+  if (items.length === 0) return null;
+
   return (
     <View className="absolute bottom-10 w-full z-50">
-      <TouchableOpacity className="bg-[#00ccbb] mx-5 rounded-lg flex-row items-center spece-x-1 p-4">
+      <TouchableOpacity
+        onPress={() => navigation.navigate("Basket")}
+        className="bg-[#00ccbb] mx-5 rounded-lg flex-row items-center spece-x-1 p-4"
+      >
         <Text className="text-white font-extrabold text-lg bg-[#01a296] py-1 px-2">
           {items.length}
         </Text>
--- screens/RestaurantScreen.js
+++ screens/RestaurantScreen.js
@@ -1,5 +1,5 @@
 import { View, Text, Image, ScrollView, TouchableOpacity } from "react-native";
-import React, { useLayoutEffect } from "react";
+import React, { useEffect, useLayoutEffect } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import {
   ArrowLeftIcon,
@@ -11,9 +11,12 @@ import {
 import { urlFor } from "../sanity";
 import DishRow from "../components/DishRow";
 import BasketIcon from "../components/BasketIcon";
+import { useDispatch } from "react-redux";
+import { setRestaurant } from "../features/restaurantSlice";
 
 const RestaurantScreen = () => {
   const navigation = useNavigation();
+  const dispatch = useDispatch();
   const {
     params: {
       id,
@@ -32,6 +35,23 @@ const RestaurantScreen = () => {
   useLayoutEffect(() => {
     navigation.setOptions({ headerShown: false });
   }, []);
+
+  useEffect(() => {
+    dispatch(
+      setRestaurant({
+        id,
+        imgUrl,
+        title,
+        rating,
+        genre,
+        address,
+        short_description,
+        dishes,
+        long,
+        lat,
+      })
+    );
+  }, [dispatch]);
 
   return (
     <>
